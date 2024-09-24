ff78600ff7cadc978a697866a63b4f3cbb059a75
Silva-Anderson0250 <anderson.silva10@alunos.sc.senac.br>
2024-09-24 00:21:37 -0300

    Não foi

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ var connectionstring = builder.Configuration.GetConnectionString("StringConexao"
 builder.Services.AddDbContext<BancoContexto>(options => options.UseSqlServer(connectionstring));
 builder.Services.AddScoped<IAlunoRepositorio, AlunoRepositorio>();
 
+
 var app = builder.Build();
 
 
